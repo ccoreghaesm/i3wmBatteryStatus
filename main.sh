1bb45d23b069b7bd6d52f7f84d14b43f9a2562c3
Ghasem Ramezani <g1999ramezani@gmail.com>
2019-02-12 17:55:13 +0330

    add flag '--i3' to use i3-nagbar for warning

--- main.sh
+++ main.sh
@@ -4,8 +4,13 @@
 while 1>0; do
 	STATUS=`cat /sys/class/power_supply/BAT0/status`
 	VALUE=`cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//'`;
-	if [ $VALUE -lt '20' ] && [ $STATUS == "Discharging" ] ; then
-		notify-send --urgency=critical "🙄🙄🙄🙄Hoy Khar ..." "⚡⚡⚡\n⚡⚡\n⚡\nChargeeeeeeeeee Naaaaaaaaaaaaaaaddddddddddaaaaaaaaaaarrrrraaaaaaaaaammmmmmmmmm\nBishoor.\n⚡\n⚡⚡\n⚡⚡⚡";
+	if [ $VALUE -lt '29' ] && [ $STATUS == "Discharging" ] ; then
+		if [ $1 == "--i3" ] ; then
+			i3-nagbar -m '🙄🙄🙄🙄 shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhaaaaarrrrrg⚡'& > /dev/null
+
+		else 
+			notify-send --urgency=critical "🙄🙄🙄🙄Hoy Khar ..." "⚡⚡⚡\n⚡⚡\n⚡\nChargeeeeeeeeee Naaaaaaaaaaaaaaaddddddddddaaaaaaaaaaarrrrraaaaaaaaaammmmmmmmmm\nBishoor.\n⚡\n⚡⚡\n⚡⚡⚡";
+		fi
 	fi
 	sleep 5;
 done

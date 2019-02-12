ce3488d2999d51e5edbf1a62dea1e5355387f477
Ghasem Ramezani <g1999ramezani@gmail.com>
2019-02-12 19:48:32 +0330

    change min power size

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 while 1>0; do
 	STATUS=`cat /sys/class/power_supply/BAT0/status`
 	VALUE=`cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//'`;
-	if [ $VALUE -lt '29' ] && [ $STATUS == "Discharging" ] ; then
+	if [ $VALUE -lt '15' ] && [ $STATUS == "Discharging" ] ; then
 		if [ $1 == "--i3" ] ; then
 			i3-nagbar -m '🙄🙄🙄🙄 shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhaaaaarrrrrg⚡'& > /dev/null
 

3a86ef1bd3316125f506fc754544e4a9a19de176
Ghasem Ramezani <g1999ramezani@gmail.com>
2019-02-13 03:07:25 +0330

    read battery status from /sys/class/power_supply/BAT0/capacity and change alarm value

--- main.sh
+++ main.sh
@@ -3,8 +3,8 @@
 # Simple Bash Script for Check Battry
 while 1>0; do
 	STATUS=`cat /sys/class/power_supply/BAT0/status`
-	VALUE=`cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//'`;
-	if [ $VALUE -lt '15' ] && [ $STATUS == "Discharging" ] ; then
+	VALUE=`cat /sys/class/power_supply/BAT0/capacity`;
+	if [ $VALUE -lt '30' ] && [ $STATUS == "Discharging" ] ; then
 		if [ $1 == "--i3" ] ; then
 			i3-nagbar -m '🙄🙄🙄🙄 shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhaaaaarrrrrg⚡'& > /dev/null
 

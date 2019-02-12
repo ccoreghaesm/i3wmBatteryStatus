db31a680f9ca541aa0e7445f9e2dab89ea40e293
Ghasem Ramezani <g1999ramezani@gmail.com>
2019-02-12 13:43:58 +0330

    write simple script to check battery status

--- main.sh
+++ main.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # Simple Bash Script for Check Battry
+while 1>0; do
+	VALUE=cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//';
+	if [ $VALUE -lt '20' ] ; then
+		notify-send "Battery" "Bezan BeCharge";
+	fi
+	sleep 5;
+done

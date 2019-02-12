551792da9dd355e5e317655f78e1d6cb0ef9aaf8
Ghasem Ramezani <g1999ramezani@gmail.com>
2019-02-12 14:43:00 +0330

    remove that stuped while loop and fix somthing i don't know

--- main.sh
+++ main.sh
@@ -3,13 +3,9 @@
 # Simple Bash Script for Check Battry
 while 1>0; do
 	STATUS=`cat /sys/class/power_supply/BAT0/status`
-	while [ $STATUS == "Charging" ] ; do
-		sleep 1m
-		STATUS=`cat /sys/class/power_supply/BAT0/status`
-	done
-	VALUE=cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//';
-	if [ $VALUE -lt '20' ] ; then
-		notify-send --urgency=critical "🙄🙄🙄🙄\n\n🙄🙄🙄🙄\nHoy Khar ..." "⚡⚡⚡\n⚡⚡\n⚡\nChargeeeeeeeeee Naaaaaaaaaaaaaaaddddddddddaaaaaaaaaaarrrrraaaaaaaaaammmmmmmmmm\nBishoor.\n⚡\n⚡⚡\n⚡⚡⚡";
+	VALUE=`cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//'`;
+	if [ $VALUE -lt '20' ] && [ $STATUS == "Discharging" ] ; then
+		notify-send --urgency=critical "🙄🙄🙄🙄Hoy Khar ..." "⚡⚡⚡\n⚡⚡\n⚡\nChargeeeeeeeeee Naaaaaaaaaaaaaaaddddddddddaaaaaaaaaaarrrrraaaaaaaaaammmmmmmmmm\nBishoor.\n⚡\n⚡⚡\n⚡⚡⚡";
 	fi
 	sleep 5;
 done

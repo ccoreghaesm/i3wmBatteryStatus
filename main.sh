3b6460107bcc77fd19fdaa04c6c144cc1684d515
Ghasem Ramezani <g1999ramezani@gmail.com>
2019-02-12 13:50:05 +0330

    read /sys/class/power_supply/BAT0/status to verify charge status... and add a simple loop to check it

--- main.sh
+++ main.sh
@@ -2,9 +2,14 @@
 
 # Simple Bash Script for Check Battry
 while 1>0; do
+	STATUS=`cat /sys/class/power_supply/BAT0/status`
+	while [ $STATUS == "Charging" ] ; do
+		sleep 1m
+		STATUS=`cat /sys/class/power_supply/BAT0/status`
+	done
 	VALUE=cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | sed 's/POWER_SUPPLY_CAPACITY=//';
 	if [ $VALUE -lt '20' ] ; then
-		notify-send "Battery" "Bezan BeCharge";
+		notify-send --urgency=critical "🙄🙄🙄🙄\n\n🙄🙄🙄🙄\nHoy Khar ..." "⚡⚡⚡\n⚡⚡\n⚡\nChargeeeeeeeeee Naaaaaaaaaaaaaaaddddddddddaaaaaaaaaaarrrrraaaaaaaaaammmmmmmmmm\nBishoor.\n⚡\n⚡⚡\n⚡⚡⚡";
 	fi
 	sleep 5;
 done
